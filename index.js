ae6e8e11cb7f759721857b339bfe43eb23af9446
Collins Kiragu <collins.kiragu@student.moringaschool.com>
2022-11-16 20:56:09 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+const employee={
+    name:"Sam",
+};
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    var newObj ={...employee};
+    newObj["streetAddress"]="11 Broadway";
+    return newObj;
+
+  }
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee["streetAddress"]="12 Broadway";
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    var newObj={...employee};
+    delete newObj["name"];
+    return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee["name"];
+    return employee;
+}
